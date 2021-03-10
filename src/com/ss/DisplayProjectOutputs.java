ef21212df43ada950e894f7497f7d769787ea737
christian <christian.angeles@smoothstack.com>
2021-03-10 09:49:29 -0800

    added class to display outputs

---
+++ src/com/ss/DisplayProjectOutputs.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.ss;
+
+import com.ss.wk1.*;
+
+/**
+ * Class used to display outputs of projects. 
+ * Note: Recode some projects for re-usability
+ * @author Christian Angeles
+ */
+public class DisplayProjectOutputs {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
