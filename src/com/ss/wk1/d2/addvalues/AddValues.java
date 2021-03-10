81ea49597ae665961f410d9a9253ed9344fbf1f1
christian <christian.angeles@smoothstack.com>
2021-03-09 20:07:49 -0800

    update description

--- src/com/ss/wk1/d2/addvalues/AddValues.java
+++ src/com/ss/wk1/d2/addvalues/AddValues.java
@@ -4,8 +4,8 @@
 package com.ss.wk1.d2.addvalues;
 
 /**
+ * Week 1, Day 2 - Add multiple user input values.
  * @author Christian Angeles
- *
  */
 public class AddValues {
 
--- src/com/ss/wk1/d2/maxnumber/MaxNumber.java
+++ src/com/ss/wk1/d2/maxnumber/MaxNumber.java
@@ -4,8 +4,8 @@
 package com.ss.wk1.d2.maxnumber;
 
 /**
+ * Week 1, Day 2 - Find the max number and index position of 2D array.
  * @author Christian Angeles
- *
  */
 public class MaxNumber {
 
