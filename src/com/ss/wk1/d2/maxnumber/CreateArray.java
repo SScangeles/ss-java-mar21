848b437fbac5ba3155ed16fdc519d549b356c134
christian <christian.angeles@smoothstack.com>
2021-03-09 20:08:25 -0800

    add CreateArray class

---
+++ src/com/ss/wk1/d2/maxnumber/CreateArray.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.ss.wk1.d2.maxnumber;
+import com.ss.wk1.d1.guessrng.RandomNumber;
+
+/**
+ * Creates a 2D array and generate random numbers (1-100) for each element.
+ * @author Christian Angeles
+ */
+public class CreateArray {
+	private int[][] arrNumbers;
+	private int row, col;
+	/**
+	 * Input number of rows and columns for 2D array.
+	 * @param row
+	 * @param col
+	 */
+	public CreateArray(int row, int col) {
+		this.row = row;
+		this.col = col;
+		RandomNumber randNum = new RandomNumber();
+		for(int i = 0; i < row; ++i) {
+			for(int j = 0; j < col; ++j) {
+				arrNumbers[i][j] = randNum.getRandNumber();
+			}
+		}
+	}
+	/**
+	 * Returns 2D array with RNG elements.
+	 * @return
+	 */
+	public int[][] getArrayNumbers() {
+		return arrNumbers;
+	}
+	/**
+	 * Returns 2D array number of rows.
+	 * @return
+	 */
+	public int getNumRows() {
+		return row;
+	}
+	/**
+	 * Return 2D array number of columns.
+	 * @return
+	 */
+	public int getNumCols() {
+		return col;
+	}
+}
