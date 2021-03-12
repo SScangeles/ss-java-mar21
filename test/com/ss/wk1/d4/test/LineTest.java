cc6c025383f6b24ab33af1a75156446c09c82fdc
christian <christian.angeles@smoothstack.com>
2021-03-12 12:05:20 -0800

    completed wk1-d4 lineTest

--- test/com/ss/wk1/d4/test/LineTest.java
+++ test/com/ss/wk1/d4/test/LineTest.java
@@ -4,6 +4,8 @@
 package com.ss.wk1.d4.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -24,18 +26,29 @@ public class LineTest {
 	}
 
 	@Test(expected = ArithmeticException.class)
-	public void getSlopeTestException() {
-		//divide by 0
+	public void getSlopeExceptionTest() {
+		//x1 = x2
 		new Line(0,0,0,1).getSlope();
 	}
 
 	@Test
 	public void getDistanceTest() {
-		
+		//distance = 0
+		assertEquals(0, new Line(0,0,0,0).getDistance(), 0);
+		//distance = 1
+		assertEquals(1, new Line(0,0,0,1).getDistance(), 0);
+		//distance = 2.236
+		assertEquals(2.236, new Line(0,0,1,2).getDistance(), 0.001);
 	}
 
 	@Test
 	public void parallelToTest() {
+		//parallel to same line
+		assertTrue(new Line(0,0,1,0).parallelTo(new Line(0,0,1,0)));
+		//parallel to different line
+		assertTrue(new Line(0,0,1,0).parallelTo(new Line(0,2,1,2)));
+		//not parallel
+		assertFalse(new Line(0,0,1,0).parallelTo(new Line(0,0,1,1)));
 		
 	}
 }
