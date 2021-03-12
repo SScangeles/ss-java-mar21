9a3d82ac38aa905576412e88e7df576a5258eecf
christian <christian.angeles@smoothstack.com>
2021-03-12 11:26:30 -0800

    update lineTest

--- test/com/ss/wk1/d4/test/LineTest.java
+++ test/com/ss/wk1/d4/test/LineTest.java
@@ -3,10 +3,35 @@
  */
 package com.ss.wk1.d4.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.ss.wk1.d4.line.Line;
+
 /**
  * @author Christian Angeles
  *
  */
 public class LineTest {
+	
+	@Test
+	public void getSlopeTest() {
+		//slope = 0
+		assertEquals(0, new Line(0,0,2,0));
+		//slope = 1
+		assertEquals(1, new Line(0,0,1,1));
+		//divide by 0
+		assertEquals(new ArithmeticException(), new Line(0,0,0,1));
+	}
 
+	@Test
+	public void getDistanceTest() {
+		
+	}
+
+	@Test
+	public void parallelToTest() {
+		
+	}
 }
