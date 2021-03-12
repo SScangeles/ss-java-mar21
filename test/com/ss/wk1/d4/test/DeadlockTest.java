4b8d3d46004512cb395317601134642690b27353
christian <christian.angeles@smoothstack.com>
2021-03-12 10:21:47 -0800

    update dlock test

--- test/com/ss/wk1/d4/test/DeadlockTest.java
+++ test/com/ss/wk1/d4/test/DeadlockTest.java
@@ -3,20 +3,25 @@
  */
 package com.ss.wk1.d4.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 import com.ss.wk1.d4.deadlock.Deadlock;
 
 /**
  * @author Christian Angeles
- *
  */
 public class DeadlockTest {
 	private Deadlock dl = new Deadlock();
-	
+
+	@Test
 	public void startTest() {
-		
+		assertEquals(true, dl.start());
 	}
-	
+
+	@Test
 	public void startNewThreadTest() {
-		
+		assertEquals(true, dl.startNewThread("ThreadName", 1, 2));
 	}
 }
--- test/com/ss/wk1/d4/test/DoubleCheckedTest.java
+++ test/com/ss/wk1/d4/test/DoubleCheckedTest.java
@@ -3,10 +3,21 @@
  */
 package com.ss.wk1.d4.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.ss.wk1.d4.doublechecked.DoubleChecked;
+
 /**
  * @author Christian Angeles
  *
  */
 public class DoubleCheckedTest {
-
+	private DoubleChecked dc = new DoubleChecked();
+	
+	@Test
+	public void startTest() {
+		assertEquals(true, dc.start());
+	}
 }
