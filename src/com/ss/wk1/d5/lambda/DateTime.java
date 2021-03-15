b412f6c80f83221c1a7bd8c114b55ea608451c94
christian <christian.angeles@smoothstack.com>
2021-03-15 13:29:31 -0700

    update datetime

--- src/com/ss/wk1/d5/lambda/DateTime.java
+++ src/com/ss/wk1/d5/lambda/DateTime.java
@@ -4,6 +4,8 @@
 package com.ss.wk1.d5.lambda;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 
 /**
  * @author Christian Angeles
@@ -14,9 +16,17 @@ public class DateTime {
 	public static void main(String[] args) {
 		
 		LocalDate date = LocalDate.now();
-		System.out.println(date.getMonth());
-		
+		System.out.println(date.getMonth()+" "+date.getDayOfMonth()+", "+date.getYear());
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm");
 		LocalTime time = LocalTime.now();
-		System.out.println(time.getHour()+":"+time.getMinute());
+		System.out.println("PST: "+time.format(dtf));
+
+		DateTimeFormatter zdtf = DateTimeFormatter.ofPattern("hh:mm");
+		ZonedDateTime zdt = ZonedDateTime.now();
+//		ZoneId est = ZoneId.of("UTC-4");
+		ZoneId est = TimeZone.getTimeZone("America/New_York").toZoneId();
+		System.out.println("EST: "+zdt.withZoneSameInstant(est).format(zdtf));
+		
 	}
 }
